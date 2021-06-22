fc3d698a7c62e7651606ed7a7c5058fa31d6fea8
Lynch4360 <lynch4360@gmail.com>
2021-06-22 14:23:33 +0000

    Created array for the game

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  //card option
+  const gameArray = [
+    {
+      name: 'chicken',
+      img: 'assets/images/chicken100.png'
+    },
+    {
+      name: 'chicken',
+      img: 'assets/images/chicken100.png'
+    },
+    {
+      name: 'cow',
+      img: 'assets/images/cow100.png'
+    },
+    {
+      name: 'cow',
+      img: 'assets/images/cow100.png'
+    },
+    {
+      name: 'duck',
+      img: 'assets/images/duck100.png'
+    },
+    {
+      name: 'duck',
+      img: 'assets/images/duck100.png'
+    },
+    {
+      name: 'goat',
+      img: 'assets/images/goat100.png'
+    },
+    {
+      name: 'goat',
+      img: 'assets/images/goat100.png'
+    },
+    {
+      name: 'rooster',
+      img: 'assets/images/rooster100.png'
+    },
+    {
+      name: 'rooster',
+      img: 'assets/images/rooster100.png'
+    },
+    {
+      name: 'turkey',
+      img: 'assets/images/turkey100.png'
+    },
+    {
+      name: 'turkey',
+      img: 'assets/images/turkey100.png'
+    },
+    {
+      name: 'sheep',
+      img: 'assets/images/sheep100.png'
+    },
+    {
+      name: 'sheep',
+      img: 'assets/images/sheep100.png'
+    },
+    {
+      name: 'pig',
+      img: 'assets/images/pig100.png'
+    },
+    {
+      name: 'pig',
+      img: 'assets/images/pig100.png'
+    },
+    {
+      name: 'rabbit',
+      img: 'assets/images/rabbit100.png'
+    },
+    {
+      name: 'rabbit',
+      img: 'assets/images/rabbit100.png'
+    }
+  ]
+})
