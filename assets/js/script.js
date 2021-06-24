abfb81554ae0beeb72f582682e98eb6291268822
Lynch4360 <lynch4360@gmail.com>
2021-06-24 12:43:13 +0000

    changed images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,96 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
 
 
   // card option
   const gameArray = [{
-      name: 'chicken',
-      img: 'assets/images/chicken100.png'
+      name: "cow",
+      img: "assets/images/cow.png"
     },
     {
-      name: 'chicken',
-      img: 'assets/images/chicken100.png'
+      name: "cow",
+      img: "assets/images/cow.png"
     },
     {
-      name: 'cow',
-      img: 'assets/images/cow100.png'
+      name: "pig",
+      img: "assets/images/pig.png"
     },
     {
-      name: 'cow',
-      img: 'assets/images/cow100.png'
+      name: "pig",
+      img: "assets/images/pig.png"
     },
     {
-      name: 'duck',
-      img: 'assets/images/duck100.png'
+      name: "chicken",
+      img: "assets/images/chicken.png"
     },
     {
-      name: 'duck',
-      img: 'assets/images/duck100.png'
+      name: "chicken",
+      img: "assets/images/chicken.png"
     },
     {
-      name: 'goat',
-      img: 'assets/images/goatzoomed100.png'
+      name: "dog1",
+      img: "assets/images/dog1.png"
     },
     {
-      name: 'goat',
-      img: 'assets/images/goatzoomed100.png'
+      name: "dog1.png",
+      img: "assets/images/dog1.png"
     },
     {
-      name: 'rooster',
-      img: 'assets/images/rooster100.png'
+      name: "dog2",
+      img: "assets/images/dog2.png"
     },
     {
-      name: 'rooster',
-      img: 'assets/images/rooster100.png'
+      name: "dog2",
+      img: "assets/images/dog2.png"
     },
     {
-      name: 'turkey',
-      img: 'assets/images/horse100.png'
+      name: "dog3",
+      img: "assets/images/dog1.png"
     },
     {
-      name: 'turkey',
-      img: 'assets/images/horse100.png'
+      name: "dog3",
+      img: "assets/images/dog3.png"
     },
     {
-      name: 'sheep',
-      img: 'assets/images/sheep100.png'
+      name: "duck",
+      img: "assets/images/duck.png"
     },
     {
-      name: 'sheep',
-      img: 'assets/images/sheep100.png'
+      name: "duck",
+      img: "assets/images/duck.png"
     },
     {
-      name: 'pig',
-      img: 'assets/images/pig100.png'
+      name: "cat",
+      img: "assets/images/cat.png"
     },
     {
-      name: 'pig',
-      img: 'assets/images/pig100.png'
+      name: "cat",
+      img: "assets/images/cat.png"
+    },
+    {
+      name: "rabbit",
+      img: "assets/images/rabbit.png"
+    },
+    {
+      name: "rabbit",
+      img: "assets/images/rabbit.png"
     }
 
   ]
   //Randomises Cards at start of game
   gameArray.sort(() => 0.5 - Math.random());
 
-  const game = document.querySelector('.game-container')
-  var pickedCards = []
-  var pickedCardsId = []
-  var cardsPaired = []
-  const displayResult = document.querySelector('#result')
+  const game = document.querySelector(".game-container")
+  let pickedCards = []
+  let pickedCardsId = []
+  let cardsPaired = []
+  const displayResult = document.querySelector("#result")
 
   // removing overlay on click
-  let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+  let overlays = Array.from(document.getElementsByClassName("overlay-text"));
 
   overlays.forEach(overlay => {
-    overlay.addEventListener('click', () => {
-      overlay.classList.remove('visible');
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("visible");
 
     });
   });
@@ -91,8 +99,8 @@ document.addEventListener('DOMContentLoaded', () => {
     constructor(totalTime) {
       this.totalTime = totalTime;
       this.timeRemaining = totalTime;
-      this.timer = document.getElementById('time-remaining');
-      this.ticker = document.getElementById('flips');
+      this.timer = document.getElementById("time-remaining");
+      this.ticker = document.getElementById("flips");
     }
     startGame() {
       this.totalClicks = 0;
@@ -105,11 +113,11 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   function makeBoard() {
     for (let i = 0; i < gameArray.length; i++) {
-      var tile = document.createElement('img');
-      tile.setAttribute('src', 'assets/images/blank100.png');
+      var tile = document.createElement("img");
+      tile.setAttribute("src", "assets/images/blank100.png");
       tile.setAttribute("class", "game-card");
-      tile.setAttribute('data-id', i);
-      tile.addEventListener('click', flipCard);
+      tile.setAttribute("data-id", i);
+      tile.addEventListener("click", flipCard);
       game.appendChild(tile);
     }
   }
@@ -119,34 +127,34 @@ document.addEventListener('DOMContentLoaded', () => {
    * checks for matches
    */
   function checkForMatch() {
-    var cards = document.querySelectorAll('img');
+    var cards = document.querySelectorAll("img");
     const optionOneId = pickedCardsId[0]
     const optionTwoId = pickedCardsId[1]
     if (pickedCards[0] === pickedCards[1]) {
-      alert('You found a match and returned our animals safely, Thank You!');
-      cards[optionOneId].setAttribute('src', 'assets/images/empty100.png');
-      cards[optionTwoId].setAttribute('src', 'assets/images/empty100.png');
+      alert("You found a match and returned our animals safely, Thank You!");
+      cards[optionOneId].setAttribute("src", "assets/images/empty100.png");
+      cards[optionTwoId].setAttribute("src", "assets/images/empty100.png");
       cardsPaired.push(pickedCards)
     } else {
-      cards[optionOneId].setAttribute('src', 'assets/images/blank100.png');
-      cards[optionTwoId].setAttribute('src', 'assets/images/blank100.png');
-      alert('Sorry, try again');
+      cards[optionOneId].setAttribute("src", "assets/images/blank100.png");
+      cards[optionTwoId].setAttribute("src", "assets/images/blank100.png");
+      alert("Sorry, try again");
     }
     pickedCards = []
     pickedCardsId = []
     displayResult.textContent = cardsPaired.length
     if (cardsPaired.length === gameArray.length / 2) {
-      displayResult.textContent = 'Thank You! You found all of our animals'
+      displayResult.textContent = "Thank You! You found all of our animals"
     }
   }
   /*
    * turns the cards around
    */
   function flipCard() {
-    var cardName = this.getAttribute('data-id');
+    var cardName = this.getAttribute("data-id");
     pickedCards.push(gameArray[cardName].name);
     pickedCardsId.push(cardName);
-    this.setAttribute('src', gameArray[cardName].img);
+    this.setAttribute("src", gameArray[cardName].img);
     if (pickedCards.length === 2) {
       setTimeout(checkForMatch, 350);
       cardsPaired.push(cardsPicked);
